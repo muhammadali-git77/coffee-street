8032653a03d8e57db8d12854f4c0edf8bf1bcc3e
Muhammadali Abdumannonov <aalixon636@gmail.com>
2025-04-02 22:13:12 +0500

    Vazifa yakunlandi

---
+++ lists.js
@@ -0,0 +1,49 @@
+const CardItem = [
+  {
+    img: "http://127.0.0.1:5500/images/vanilla.png",
+    title: "Vannilla Latte",
+    ordered: 21
+  },
+  {
+    img: "http://127.0.0.1:5500/images/espresso.png",
+    title: "Espresso",
+    ordered: 13
+  },
+  {
+    img: "http://127.0.0.1:5500/images/hazelnut%20latte.png",
+    title: "Hazelnut Latte",
+    ordered: 3
+  },
+  {
+    img: "http://127.0.0.1:5500/images/turkish.png",
+    title: "Turkish",
+    ordered: 33
+  },
+  {
+    img: "http://127.0.0.1:5500/images/americano.png",
+    title: "Americano",
+    ordered: 24
+  },
+  {
+    img: "http://127.0.0.1:5500/images/arabica.png",
+    title: "Arabica",
+    ordered: 18
+  },
+  {
+    img: "http://127.0.0.1:5500/images/mocha.png",
+    title: "Mocha",
+    ordered: 20
+  },
+  {
+    img: "http://127.0.0.1:5500/images/affgatto.png",
+    title: "Affgatto",
+    ordered: 12
+  },
+  {
+    img: "http://127.0.0.1:5500/images/risretto.png",
+    title: "Risretto",
+    ordered: 10
+  },
+];
+
+export default CardItem;
